0bc41f78d03523c516cc388619ad463e95bc2208
Lý Vĩnh Thuận <133451381+ThuanLy-0092@users.noreply.github.com>
2024-09-27 21:49:48 +0700

    Update rag_llm.py

--- rag_llm.py
+++ rag_llm.py
@@ -1,57 +1,43 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig, AutoModelForSequenceClassification
 from langchain.prompts import PromptTemplate
 from langchain.embeddings import HuggingFaceInferenceAPIEmbeddings
-from langchain.chains import RetrievalQA, ConversationalRetrievalChain
+from langchain.chains import RetrievalQA
 from qdrant_client import QdrantClient
 from langchain_qdrant import QdrantVectorStore
-from langchain.llms import HuggingFacePipeline
-import torch
-from langchain_community.llms import CTransformers
 from langchain_groq import ChatGroq
 from langchain.retrievers import ContextualCompressionRetriever
-from langchain.retrievers.document_compressors import CrossEncoderReranker
-from langchain_community.cross_encoders import HuggingFaceCrossEncoder
 from langchain.chains import LLMChain, HypotheticalDocumentEmbedder
 from langchain_community.document_transformers import LongContextReorder
 from langchain.retrievers.document_compressors import DocumentCompressorPipeline
-from langchain.chains.question_answering import load_qa_chain
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import LLMChain
 from langchain_qdrant import FastEmbedSparse, RetrievalMode
 
-
-
 class RAGPipelineSetup:
-    def __init__(self, qdrant_url, qdrant_api_key, qdrant_collection_name, huggingface_api_key, embeddings_model_name, groq_api_key,groq_api_key2,groq_api_key3, reranker_model_name):
+    def __init__(self, qdrant_url, qdrant_api_key, qdrant_collection_name, huggingface_api_key, embeddings_model_name, groq_api_key):
         self.QDRANT_URL = qdrant_url
         self.QDRANT_API_KEY = qdrant_api_key
         self.QDRANT_COLLECTION_NAME = qdrant_collection_name
         self.HUGGINGFACE_API_KEY = huggingface_api_key
         self.EMBEDDINGS_MODEL_NAME = embeddings_model_name
         self.GROQ_API_KEY = groq_api_key
-        self.GROQ_API_KEY2 = groq_api_key2
-        self.GROQ_API_KEY3 = groq_api_key3
-        self.RERANKER_MODEL_NAME = reranker_model_name
         self.current_source = None
         self.rag_pipeline = None
         self.embeddings = self.load_embeddings()
         self.retriever = None
         self.pipe = None
         self.prompt = self.load_prompt_template()
-
     def load_embeddings(self):
         bge_embeddings = HuggingFaceInferenceAPIEmbeddings(
             model_name=self.EMBEDDINGS_MODEL_NAME,
             api_key=self.HUGGINGFACE_API_KEY
         )
         llm = ChatGroq(
-         temperature=0, 
-         groq_api_key=self.GROQ_API_KEY, 
-         model_name="gemma2-9b-it"
-         )
-        embeddings = HypotheticalDocumentEmbedder.from_llm(llm, bge_embeddings, prompt_key="web_search")
+        temperature=0.3, 
+        groq_api_key=self.GROQ_API_KEY, 
+        model_name="gemma2-9b-it",
+        max_tokens=50
+        )
+        embeddings=HypotheticalDocumentEmbedder.from_llm(llm,bge_embeddings,prompt_key="web_search")
         return embeddings
-
+    
     def load_retriever(self, retriever_name, embeddings):
       # Tạo client kết nối với Qdrant
       client = QdrantClient(
@@ -73,8 +59,7 @@ class RAGPipelineSetup:
 
       # Tạo retriever từ Qdrant với top k kết quả ban đầu
       base_retriever = db.as_retriever(search_kwargs={"k": 5},search_type="mmr")
-
-    #   # Tạo mô hình reranker với HuggingFaceCrossEncoder
+       #   # Tạo mô hình reranker với HuggingFaceCrossEncoder
     #   model = HuggingFaceCrossEncoder(model_name=self.RERANKER_MODEL_NAME)
     #   reranker = CrossEncoderReranker(model=model, top_n=5)
 
@@ -96,15 +81,15 @@ class RAGPipelineSetup:
 
     def load_model_pipeline(self, max_new_tokens=1024):
         llm = ChatGroq(
-            temperature=0, 
-            groq_api_key=self.GROQ_API_KEY2, 
+            temperature=0.3, 
+            groq_api_key=self.GROQ_API_KEY, 
             model_name="llama3-groq-70b-8192-tool-use-preview"
         )
         return llm
-
     def load_prompt_template(self):
         query_template = '''Bạn là trợ lý ảo thông minh hỗ trợ giải đáp câu hỏi cho sinh viên, 
-                            hãy dựa vào context được cung cấp và 1 ít kiến thức của bạn để trả lời câu hỏi của người dùng bằng Tiếng Việt (context),
+                            hãy dựa vào context được cung cấp và 1 ít kiến thức của bạn để trả lời câu hỏi của người dùng bằng Tiếng Việt,
+                            bạn cũng sẽ được cung cấp 5 tin nhắn gần nhất của người dùng để hiểu ngữ cảnh
                             nếu bạn không có câu trả lời hãy gợi ý cách tìm ra được thông tin.
                             Hãy chọn lọc thông tin để cung cấp câu trả lời ngắn gọn nhưng đầy đủ cho người dùng
                             Trong văn bản được cung cấp sẽ có những từ được nối bằng _ nhưng bạn không cần giữ nguyên ký tự _  
@@ -112,41 +97,14 @@ class RAGPipelineSetup:
                             \n### Context:{context} \n\n### Người dùng: {question}'''
         prompt = PromptTemplate(template=query_template, input_variables=["context", "question"])
         return prompt
-
     def load_rag_pipeline(self, llm, retriever, prompt):
-        # Tạo QA chain với load_qa_chain
-        qa_chain = load_qa_chain(llm, chain_type="stuff",prompt=prompt)  
-        memory = ConversationBufferMemory(memory_key="chat_history",k=0, return_messages=True)
-
-        _template ="""Bạn là một trợ lý thông minh. Nhiệm vụ của bạn là viết lại câu hỏi hiện tại một cách rõ ràng và dễ hiểu nhất có thể. Hãy ưu tiên giữ nguyên nội dung chính của câu hỏi hiện tại và chỉ sử dụng ngữ cảnh từ lịch sử trò chuyện nếu nó giúp làm rõ thêm ý nghĩa của câu hỏi. Không tự động thay đổi câu hỏi hiện tại dựa trên ngữ cảnh trước trừ khi thực sự cần thiết.
-
-        ### Câu hỏi hiện tại:
-        {question}
-
-        ### Lịch sử cuộc trò chuyện (nếu cần):
-        {chat_history}
-
-        Viết lại câu hỏi hiện tại sao cho ngắn gọn, rõ ràng,bằng tiếng việt nhưng vẫn giữ nguyên ý nghĩa chính của câu hỏi gốc. Chỉ sử dụng lịch sử cuộc trò chuyện nếu nó thực sự cần thiết để làm rõ câu hỏi của người dùng.
-
-        """
-        condense_question_prompt_template = PromptTemplate.from_template(_template)
-
-        LLM = ChatGroq(
-            temperature=0, 
-            groq_api_key=self.GROQ_API_KEY3, 
-            model_name="gemma2-9b-it"
-        )
-        question_generator = LLMChain(llm=LLM, prompt=condense_question_prompt_template, memory=memory)
-        # Tạo ConversationalRetrievalChain với các tham số cần thiết
-        conversational_chain = ConversationalRetrievalChain(
+        retrieval_qa = RetrievalQA.from_chain_type(
             retriever=retriever,
-            combine_docs_chain=qa_chain,   # Chuỗi kết hợp tài liệu với mô hình LLM
-            question_generator=question_generator,       
-            return_source_documents=False,
-            memory=memory     
+            chain_type="stuff",
+            llm=llm,
+            chain_type_kwargs={'prompt': prompt}
         )
-        return conversational_chain
-
+        return retrieval_qa
     def rag(self, source):
         if source == self.current_source:
             return self.rag_pipeline
