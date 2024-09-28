28ba37743715950c9352be8622a4a3e643d9c3f4
Lý Vĩnh Thuận <133451381+ThuanLy-0092@users.noreply.github.com>
2024-09-28 14:09:50 +0700

    Update API.py

--- API.py
+++ API.py
@@ -2,16 +2,19 @@ from fastapi import FastAPI, Request
 from pydantic import BaseModel
 import uvicorn
 from rag_llm import RAGPipelineSetup
+from typing import List
 
 app = FastAPI()
 
-# Cấu hình FastAPI với các thông số giống như trong ứng dụng Streamlit của bạn
 EMBEDDINGS_MODEL_NAME = "BAAI/bge-m3"
 QDRANT_URL = "https://88bb6378-66e7-49db-a5de-6bb17f0d664a.europe-west3-0.gcp.cloud.qdrant.io:6333"
-HUGGINGFACE_API_KEY = "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"
+HUGGINGFACE_API_KEY = "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"
 QDRANT_API_KEY = "cLGVHbp48h0CZayJIXdxVW-JJijODOKpBFlzIPm6nvLHxRE4B_nrFA"
+GROQ_API_KEY= "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"
+GROQ_API_KEY2= "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"
+GROQ_API_KEY3= "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"
 RERANKER_MODEL_NAME = "BAAI/bge-reranker-v2-m3"
-GROQ_API_KEY= "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"
+
 DATABASE_TO_COLLECTION = {
     "Trường Đại học Khoa học Tự nhiên": "US_vectorDB",
     "Trường Đại học Công nghệ Thông tin": "UIT_vectorDB",
@@ -35,16 +38,19 @@ async def query(prompt_request: PromptRequest):
         huggingface_api_key=HUGGINGFACE_API_KEY,
         embeddings_model_name=EMBEDDINGS_MODEL_NAME,
         groq_api_key=GROQ_API_KEY,
+        groq_api_key2=GROQ_API_KEY2,
+        groq_api_key3=GROQ_API_KEY3,
+        reranker_model_name=RERANKER_MODEL_NAME 
     )
+
     rag_pipeline = rag_setup.rag(source=selected_collection)
-    response = rag_pipeline.run(prompt_request.prompt)
-    return {"response": response}
+    response = rag_pipeline({"question": prompt_request.prompt})
+    return {"query": response["answer"]}
 
 
 @app.head("/api/query")
 async def query_head(request: Request):
-    # Respond with a 200 OK status for HEAD requests
     return {"status": "HEAD request successful"}
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
