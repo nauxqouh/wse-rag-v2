a046d014a08125487dcb3235fc8c7da54393bb00
Lý Vĩnh Thuận <133451381+ThuanLy-0092@users.noreply.github.com>
2024-10-13 14:00:26 +0700

    Update api_rag.py

--- api_rag.py
+++ api_rag.py
@@ -109,7 +109,6 @@ class RAGPipelineSetup:
                             hãy dựa vào context được cung cấp và 1 ít kiến thức của bạn để trả lời câu hỏi của người dùng bằng Tiếng Việt (context),
                             nếu bạn không có câu trả lời hãy gợi ý cách tìm ra được thông tin.
                             Trong câu trả lời của bạn không được dùng các từ như "từ văn bản được cung cấp, "văn bản được đề cập", ...
-                            Khi thiếu thông tin hãy dùng thông tin từ các câu hỏi trước, nếu không thì chỉ tập trung vào câu hỏi hiện tại
                             \n### Context:{context} \n\n### {question}'''
         prompt = PromptTemplate(template=query_template, input_variables=["context", "question"])
         return prompt
